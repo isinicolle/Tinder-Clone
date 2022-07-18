9c3e3b636a4d2c5a1753bb0f8ea652d9cd73a738
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-17 21:55:17 -0600

    .
    .

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-web'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  //constante para la navigation
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Go to chat"
+      onPress={() => { navigation.navigate('Chat') }} />
     </View>
   )
 }
